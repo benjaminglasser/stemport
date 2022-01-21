a82d14e052eb64752c0c04c62aaf40e0c2a83861
ben glasser <ben@fantasticvoyage.com>
2022-01-21 11:15:54 -0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h4 className="display-4 text-center mb-4">
+        Stemport
+      </h4>
+      <h5 className="display-8 text-center mb-4">
+        Analyze - Organize - Import
+      </h5>
+    </div>
+  );
+}
+
+export default App;
