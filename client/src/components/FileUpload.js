faf10bd8d36e3ee1af0f1433ed8b579f82863f38
ben glasser <ben@fantasticvoyage.com>
2022-01-21 17:35:15 -0800

    checkpoint

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,21 +1,65 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import axios from 'axios'
+import { useDropzone } from 'react-dropzone'
 
-const FileUpload = () => {
+const FileUpload = (props) => {
 
-    const [file, setFile] = useState('');
-    const [filename, setFilename] = useState('')
-    const [uploadedFile, setUploadedFile] = useState({});
+    const [uploadFiles, setUploadFiles] = useState([]);
+    // const [filename, setFilename] = useState('')
+    // const [uploadedFile, setUploadedFile] = useState({});
 
-    const onChange = e => {
-        setFile(e.target.files[0]);
-        // setFilename(e.target.files[0].name);
-    }
+    // const [progress, setProgress] = useState(0);
+    // const [isUploading, setUploading] = useState(false);
+
+    // const { fileRejections,
+    //     isDragActive,
+    //     acceptedFiles,
+    //     isDragAccept,
+    //     isDragReject,
+    //     getRootProps,
+    //     getInputProps } = useDropzone({
+    //         onDrop: (acceptedFiles) => {
+    //             setUploadFiles((...prevFiles) =>
+    //                 acceptedFiles.reduce(
+    //                     (acc, file) => ({
+    //                         ...acc,
+    //                         [file.name]: {
+    //                             file
+    //                         }
+    //                     }),
+    //                     ...prevFiles
+    //                 )
+    //             );
+    //         },
+    //         accept: ".jpg,.jpeg,.mp3,.wav"
+    //     });
+    const onDrop = useCallback(acceptedFiles => {
+        setUploadFiles(acceptedFiles)
+    }, [])
+
+    const { getRootProps, getInputProps } = useDropzone({ onDrop })
+
+
+    const files = uploadFiles.map(file => (
+        <li key={file.path}>
+            {file.path} - {file.size} bytes
+        </li>
+    ));
+
+
+    // const onChange = e => {
+    //     setUploadFiles(e.target.uploadFiles);
+    //     // setFilename(e.target.files[0].name);
+    // }
 
     const onSubmit = async e => {
         e.preventDefault();
+
         const formData = new FormData();
-        formData.append('file', file);
+
+        for (let i = 0; i < uploadFiles.length; i++) {
+            formData.append(`files[${i}]`, uploadFiles[i]);
+        }
 
         try {
             const res = await axios.post('/upload', formData, {
@@ -24,9 +68,9 @@ const FileUpload = () => {
                 }
             });
 
-            const { fileName, filePath } = res.data;
+            // const { fileName, filePath } = res.data;
 
-            setUploadedFile({ fileName, filePath })
+            // setUploadedFile({ fileName, filePath })
 
         } catch (err) {
             if (err.response.status === 500) {
@@ -40,16 +84,23 @@ const FileUpload = () => {
     return <div>
         <form onSubmit={onSubmit}>
 
-            <div className="input-group mb-4">
-                <input type="file" className="form-control" id="customFile" onChange={onChange} />
-                {/* <label className="input-group-text" htmlFor="customFile">Load Stem</label> */}
-            </div>
-
+            <section className="container">
+                <div {...getRootProps({ className: 'dropzone' })}>
+                    <input {...getInputProps()} />
+                    <p>Drag 'n' drop some files here, or click to select files</p>
+                    <p>(only accepts .mp3 or .mov)</p>
+                </div>
+                <aside>
+                    <h4>Files</h4>
+                    <ul>{files}</ul>
+                </aside>
+            </section>
 
             <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4" />
         </form>
 
     </div>;
 };
+
 
 export default FileUpload;
--- server.js
+++ server.js
@@ -11,16 +11,20 @@ app.post('/upload', (req, res) => {
         return res.status(400).json({ msg: "No file uploaded" });
     }
 
-    const file = req.files.file;
 
-    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-        if (err) {
-            console.log(err);
-            return res.status(500).send(err);
-        }
+    console.log(req.files);
 
-        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-    });
+
+    // const file = req.files.file;
+
+    // file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    //     if (err) {
+    //         console.log(err);
+    //         return res.status(500).send(err);
+    //     }
+
+    //     res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+    // });
 });
 
 app.listen(5000, () => console.log('Server Started...'));
